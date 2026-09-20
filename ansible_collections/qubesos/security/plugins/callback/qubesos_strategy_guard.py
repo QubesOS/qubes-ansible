b4d86d0036062ccbe3967c1eec33d382dce9e660
Guillaume Chinal <guiiix@invisiblethingslab.com>
2026-09-20 15:33:05 +0200

    fix: remove deprecated argument include_delegate_to
    This argument is deprecated, removed in recent  Ansible
    versions and is not required in strategy guard plugin.
    
    https://github.com/ansible/ansible/pull/84306

--- ansible_collections/qubesos/security/plugins/callback/qubesos_strategy_guard.py
+++ ansible_collections/qubesos/security/plugins/callback/qubesos_strategy_guard.py
@@ -83,7 +83,6 @@ class CallbackModule(CallbackBase):
             host=host,
             task=task,
             include_hostvars=True,
-            include_delegate_to=True,
         ).get("ansible_connection", task.connection)
 
         if current_connection == "qubes":
